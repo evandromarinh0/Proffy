4827d83738eb87d3bf71fbb8224fde2449fefd4d
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-01-06 10:58:12 -0300

    routes up to date

--- mobileproffy/src/routes/index.tsx
+++ mobileproffy/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
 import GiveClasses from '../pages/GiveClasses';
+import StudyTabs from './StudyTabs';
 
 const Stack = createStackNavigator();
 
@@ -11,6 +12,7 @@ const StackRoutes: React.FC = () => {
     <Stack.Navigator screenOptions={{ headerShown: false, cardStyle:{ backgroundColor: '#9871f5' } }}>
       <Stack.Screen name="Landing" component={Landing} />
       <Stack.Screen name="GiveClasses" component={GiveClasses} />
+      <Stack.Screen name="Study" component={StudyTabs} />
     </Stack.Navigator>
   );
 }
