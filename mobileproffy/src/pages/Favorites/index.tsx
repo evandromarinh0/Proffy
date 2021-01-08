0ecd71f704d356e59c1a636bb5924f43facbc702
Evandro Marinho <evanforgotlastone@hotmail.com>
2021-01-07 21:58:19 -0300

    favorites being cached and uncached - good to go

--- mobileproffy/src/pages/Favorites/index.tsx
+++ mobileproffy/src/pages/Favorites/index.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import React, { useEffect, useState } from 'react';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import PageHeader from '../../components/PageHeader';
-import TeacherItem from '../../components/TeacherItem';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
 
 import { Container } from './styles';
 
 const Favorites: React.FC = () => {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    AsyncStorage.getItem('favorites').then(response => {
+      if (response) {
+        const favTeachers = JSON.parse(response);
+        
+        setFavorites(favTeachers);
+      }
+    });
+  }, [favorites]);
+
   return (
     <Container>
       <PageHeader title="Meus proffys favoritos" />
 
       <ScrollView style={{ marginTop: -60 }} contentContainerStyle={{ padding: 20 }}>
-        <TeacherItem />
-        <TeacherItem />
-        <TeacherItem />
+        {favorites.map((teacher: Teacher) => (
+          <TeacherItem key={teacher.id} teacher={teacher} fav={true } />
+        ))}
       </ScrollView>
     </Container>
   );
